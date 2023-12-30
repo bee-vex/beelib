608ba900fc6312430239adb460531fb8a66c95e4
Maddy Scott <austinjs1213@gmail.com>
2023-12-30 14:42:52 -0500

    Update include/bee/control/controller.hpp
    Co-authored-by: Abinav Kumar <132693471+AbeetoChanito@users.noreply.github.com>

--- include/bee/control/controller.hpp
+++ include/bee/control/controller.hpp
@@ -6,6 +6,7 @@ namespace bee {
 template <typename Input, typename Output> class Controller {
     public:
         virtual Output update(Input input) = 0;
+        
         virtual void reset() = 0;
 };
 } // namespace bee
