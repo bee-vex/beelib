114c4f23c29e8784515566630cea73f44bcd91d5
sufferiing <austinjs1213@gmail.com>
2023-12-31 20:54:09 -0500

    Formatting

--- include/bee/settler/combined.hpp
+++ include/bee/settler/combined.hpp
@@ -3,27 +3,22 @@
 #include <functional>
 
 namespace bee {
-    
-template <typename... Settlers> class CombinedSettlers
-    : public Settler {
+
+template <typename... Settlers> class CombinedSettlers : public Settler {
         std::tuple<Settlers...> settlers;
         std::function<bool(std::tuple<Settlers...>)> combine;
     public:
-        static_assert(
-            (traits::is_settler<Settlers>::value && ...),
-            "Settler types passed to CombinedSettlers must implement the Settler interface");
+        static_assert((traits::is_settler<Settlers>::value && ...),
+                      "Settler types passed to CombinedSettlers must implement the Settler interface");
 
-        CombinedSettlers(std::tuple<Settlers...> settlers,
-                            std::function<bool(std::tuple<Settlers...>)> combine)
+        CombinedSettlers(std::tuple<Settlers...> settlers, std::function<bool(std::tuple<Settlers...>)> combine)
             : settlers(settlers),
               combine(combine) {}
 
-        bool isSettled() override {
-            return combine(settlers);
-        }
+        bool isSettled() override { return combine(settlers); }
 
         void reset() override {
             std::apply([](auto&... tuple) { (tuple.reset(), ...); }, settlers);
         }
 };
-}
+} // namespace bee
